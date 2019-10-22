54a9991bc13d92c92663e5ed2977f938c8b45f58
Nathaniel McCallum <npmccallum@redhat.com>
2019-10-22 14:55:26 +0300

    Finish initial setup
    Signed-off-by: Nathaniel McCallum <npmccallum@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 #![deny(missing_docs)]
 //! Dummy crate needs high-level documentation.
 /// Dummy public function needs documentation.
-pub fn it_works() {
-    assert!(true);
-}
+pub fn it_works() {}
